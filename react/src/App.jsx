700ec17aec117cf530ff2b653b35a1ee760446da
Julien-G-Man <juliengmanana@gmail.com>
2025-12-21 04:08:54 +0100

    Modify ping

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -10,8 +10,20 @@ const BASE_API_URL = import.meta.env.VITE_BASE_API_URL || "http://localhost:8000
 
 function App() {
   useEffect(() => {
-    // Ping backend once on load 
+    // Immediate Ping to wake up backend server
     pingHealth(BASE_API_URL);
+
+    // Set the "Keep-Alive" heartbeat for every 10 minutes
+    const intervalId = setInterval(() => {
+      console.log("Heartbeat ping sent...");
+      pingHealth(BASE_API_URL);
+    }, 600000); // 10 minutes = 600,000ms
+
+    // THE CLEANUP
+    return () => {
+      console.log("Cleaning up old heartbeats");
+      clearInterval(intervalId);
+    };
   }, []);
 
   return (
